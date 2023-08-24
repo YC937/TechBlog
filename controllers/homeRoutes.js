6d02a464302d8cfd5fe60c512440daab0eec4c84
Circadian <circadian@iMac.local>
2023-08-24 15:53:30 -0400

    code added

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Blogpost, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogpostData = await Blogpost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['message']
+                },
+            ],
+        });
+
+        const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+
+        res.render('homepage', {
+            blogposts,
+            logged_in: req.session.logged_in,
+            username: req.session.username
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blogpost/:id', async (req, res) => {
+    try {
+      const blogpostData = await Blogpost.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      const blogpost = blogpostData.get({ plain: true });
+  
+      res.render('blogpost', {
+        ...blogpost,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // Use withAuth middleware to prevent access to route
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Blogpost }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true,
+        username: req.session.username,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
